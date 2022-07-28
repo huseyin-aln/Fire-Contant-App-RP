5b1c8dc0109abc77b5e0362af4135863249d27bd
huseyin <huseyinarslan06@gmail.com>
2022-07-28 16:17:17 +0300

    fixed

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHNNWd4beJg3onJqjIGM0csQ_papIANa4",
-  authDomain: "fire-contact-app-e03f3.firebaseapp.com",
-  projectId: "fire-contact-app-e03f3",
-  storageBucket: "fire-contact-app-e03f3.appspot.com",
-  messagingSenderId: "759110307639",
-  appId: "1:759110307639:web:d02267c1d474050455d4b2",
-  measurementId: "G-TGWCNSKGLG",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
 
 const app = initializeApp(firebaseConfig);
